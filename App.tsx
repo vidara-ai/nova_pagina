e8dc77066e8a970c76da5071d8cc72c30ebf34f4
vidara-ai <fluxxads@gmail.com>
2026-02-09 19:38:59 -0300

    Add files via upload
    4

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import PrivateRoute from './PrivateRoute';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
@@ -11,6 +12,9 @@ const App: React.FC = () => {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
+          {/* Rota pública inicial */}
+          <Route path="/" element={<Home />} />
+          
           {/* Rota pública para login */}
           <Route path="/login" element={<Login />} />
           
@@ -25,7 +29,7 @@ const App: React.FC = () => {
           />
           
           {/* Redirecionamento padrão */}
-          <Route path="*" element={<Navigate to="/login" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
